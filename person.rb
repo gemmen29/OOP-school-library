d893b055f13af28185ef6bc45039331333a1a1d9
George Magdy <georgtriple@gmail.com>
2022-02-28 19:48:59 +0200

    Fix linters errors

--- person.rb
+++ person.rb
@@ -1,24 +1,26 @@
 class Person
-  def initialize(age, name: "Unknown", parent_permission: true)
+  def initialize(age, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  attr_reader :id, :name, :parent_permission
-  attr_writer :name, :age
+  attr_accessor :name
+  attr_reader :id, :parent_permission
+  attr_writer :age
 
   private
-    def is_of_age?
-      if @age >= 18
-        return true
-      end
-      return false
-    end
+
+  def of_age?
+    return true if @age >= 18
+
+    false
+  end
 
   public
-    def can_use_services?
-      is_of_age? || @parent_permission
-    end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 end
--- test.rb
+++ test.rb
@@ -2,24 +2,22 @@ require_relative './person'
 require_relative './teacher'
 require_relative './student'
 
-
 person1 = Person.new(15)
-p (person1)
-p (person1.can_use_services?)
+p(person1)
+p(person1.can_use_services?)
 
 person2 = Person.new(15, parent_permission: false)
-p (person2)
-p (person2.can_use_services?)
+p(person2)
+p(person2.can_use_services?)
 
 person3 = Person.new(18, parent_permission: false)
-p (person3)
-p (person3.can_use_services?)
+p(person3)
+p(person3.can_use_services?)
 
-student = Student.new(15, "A", name: "Student")
-p (student)
-p (student.play_hooky)
+student = Student.new(15, 'A', name: 'Student')
+p(student)
+puts(student.play_hooky)
 
-teacher = Student.new(15, "CS", name: "Teacher")
-p (teacher)
-p (teacher.can_use_services?)
-
+teacher = Student.new(15, 'CS', name: 'Teacher')
+p(teacher)
+p(teacher.can_use_services?)
