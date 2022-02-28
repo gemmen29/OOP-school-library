7d46801f5539edc989666e0f2f00d3f7d066ea8f
George Magdy <georgtriple@gmail.com>
2022-02-28 18:31:24 +0200

    Add getter for id, name, and parent_permission

--- person.rb
+++ person.rb
@@ -1,8 +1,10 @@
 class Person
-    def initialize(age, name = "Unknown", parent_permission = "true")
-      @id = Random.rand(1..1000)
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
+  def initialize(age, name = "Unknown", parent_permission = "true")
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
+
+  attr_reader :id, :name, :parent_permission
 end
