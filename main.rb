de37ff77310fdc948d076f4bf47792b1713437e9
George Magdy <georgtriple@gmail.com>
2022-03-03 18:02:17 +0200

    Display options

--- main.rb
+++ main.rb
@@ -1,11 +1,23 @@
-def App
-  def run()
+class App
+  def run
+    options
   end
+
+  private
+    def options
+      puts "1 - List all books"
+      puts "2 - List all people"
+      puts "3 - Create a person"
+      puts "4 - Create a book"
+      puts "5 - Create a rental"
+      puts "6 - List all rentals for a given person id"
+      puts "7 - Exit"
+    end
 end
 
 def main
-  app = App.new()
-  app.run()
+  app = App.new
+  app.run
 end
 
-main()
+main
